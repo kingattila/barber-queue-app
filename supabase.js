7445c176bcaf1b018a055b0942c5b3b86afccef7
Attila Keresztesi <attilakeresztesi@MacBook-Pro.local>
2025-07-14 14:01:30 +0930

    Fix: correct Supabase anon key

--- supabase.js
+++ supabase.js
@@ -2,6 +2,6 @@
 import { createClient } from 'https://cdn.jsdelivr.net/npm/@supabase/supabase-js/+esm'
 
 const SUPABASE_URL = 'https://ylsodxvamvpauwlwizkf.supabase.co'
-const SUPABASE_ANON_KEY = 'eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJpc3MiOiJzdXBhYmFzZSIsInJlZiI6Inlsc29keHZhbXZwYXV3bHdpemtmIiwicm9sZSI6InNlcnZpY2Vfcm9sZSIsImlhdCI6MTc1MjQ2MDUzNiwiZXhwIjoyMDY4MDM2NTM2fQ.YYeZyaM7D5G6PMMsUnqoKyVgXn9B-RWCgFq2F_HZghw'
+const SUPABASE_ANON_KEY = 'sb_publishable_j3FUOBH0BUpg3WIsGaZpnw_t_9o-pQF'
 
 export const supabase = createClient(SUPABASE_URL, SUPABASE_ANON_KEY)
