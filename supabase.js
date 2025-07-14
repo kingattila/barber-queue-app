a9c0bd6eeaf4248d352a53cfda00c7aaf24d8251
Attila Keresztesi <attilakeresztesi@MacBook-Pro.local>
2025-07-14 13:46:26 +0930

    🔐 Update Supabase anon key

--- supabase.js
+++ supabase.js
@@ -1,7 +1,7 @@
 // supabase.js
 import { createClient } from 'https://cdn.jsdelivr.net/npm/@supabase/supabase-js/+esm'
 
-const SUPABASE_URL = 'https://ylsodxvamvpauwlwizkf.supabase.co/'
-const SUPABASE_ANON_KEY = 'eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJpc3MiOiJzdXBhYmFzZSIsInJlZiI6Inlsc29keHZhbXZwYXV3bHdpemtmIiwicm9sZSI6ImFub24iLCJpYXQiOjE3NTE4NDY1MDYsImV4cCI6MjA2NzQyMjUwNn0.4k0PjrPMugDCOS_Y5SuCpm6u-ycSXmO75xqxdHCtW4c'
+const SUPABASE_URL = 'https://ylsodxvamvpauwlwizkf.supabase.co'
+const SUPABASE_ANON_KEY = 'eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJpc3MiOiJzdXBhYmFzZSIsInJlZiI6Inlsc29keHZhbXZwYXV3bHdpemtmIiwicm9sZSI6ImFub24iLCJpYXQiOjE3NTI0NjA1MzYsImV4cCI6MjA2ODAzNjUzNn0.JMtzBrayxLnLwxXgZKXQVnxhSJcRnqD-OK4bnyjkpNQ'
 
 export const supabase = createClient(SUPABASE_URL, SUPABASE_ANON_KEY)
